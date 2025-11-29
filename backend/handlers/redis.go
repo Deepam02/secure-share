b44425939d2308aefb37c6f5028186fc9f71377d
Deepam Goyal <deepam02goyal@gmail.com>
2025-11-29 20:20:21 +0530

    test redis connection
    Signed-off-by: Deepam Goyal <deepam02goyal@gmail.com>

--- backend/handlers/redis.go
+++ backend/handlers/redis.go
@@ -2,6 +2,7 @@ package handlers
 
 import (
 	"context"
+	"net/http"
 	"os"
 
 	"github.com/redis/go-redis/v9"
@@ -15,4 +16,13 @@ func InitRedis() {
 		Addr:     os.Getenv("REDIS_HOST"),
 		Password: os.Getenv("REDIS_PASSWORD"),
 	})
+}
+
+func TestRedisHandler(w http.ResponseWriter, r *http.Request) {
+	err := Rdb.Set(ctx, "hello", "world", 0).Err()
+	if err != nil {
+		w.Write([]byte("Redis error: " + err.Error()))
+		return
+	}
+	w.Write([]byte("Redis OK"))
 }
--- backend/main.go
+++ backend/main.go
@@ -11,6 +11,7 @@ func main() {
 
 	http.HandleFunc("/upload", handlers.UploadHandler)
 	http.HandleFunc("/download/", handlers.DownloadHandler)
+	http.HandleFunc("/test", handlers.TestRedisHandler)
 
 	log.Println("Server starting on port 10000")
 	http.ListenAndServe(":10000", nil)
