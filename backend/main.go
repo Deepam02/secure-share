c652fa613b0cb89e75576dd3571fadf53a6465b2
Deepam Goyal <deepam02goyal@gmail.com>
2025-11-29 21:54:38 +0530

    fix backend code
    Signed-off-by: Deepam Goyal <deepam02goyal@gmail.com>

--- backend/main.go
+++ backend/main.go
@@ -6,13 +6,28 @@ import (
 	"secure-share/handlers"
 )
 
+func withCORS(h http.HandlerFunc) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		// allow your Vercel domain here in production
+		w.Header().Set("Access-Control-Allow-Origin", "*")
+		w.Header().Set("Access-Control-Allow-Methods", "GET, POST, OPTIONS")
+		w.Header().Set("Access-Control-Allow-Headers", "Content-Type")
+
+		if r.Method == http.MethodOptions {
+			w.WriteHeader(http.StatusNoContent)
+			return
+		}
+
+		h(w, r)
+	}
+}
+
 func main() {
 	handlers.InitRedis()
 
-	http.HandleFunc("/upload", handlers.UploadHandler)
-	http.HandleFunc("/get-files/", handlers.GetFilesHandler)
-	http.HandleFunc("/download/", handlers.DownloadSpecificHandler)
-	http.HandleFunc("/download-zip/", handlers.DownloadZipHandler)
+	http.HandleFunc("/upload", withCORS(handlers.UploadHandler))
+	http.HandleFunc("/meta/", withCORS(handlers.MetaHandler))
+	http.HandleFunc("/file/", withCORS(handlers.FileHandler))
 
 	log.Println("Server running on port 10000")
 	http.ListenAndServe(":10000", nil)
