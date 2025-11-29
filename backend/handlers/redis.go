1764a4af1931aec2a4c5cb329c987da6e4823de6
Deepam Goyal <deepam02goyal@gmail.com>
2025-11-29 20:28:13 +0530

    redis debug
    Signed-off-by: Deepam Goyal <deepam02goyal@gmail.com>

--- backend/handlers/redis.go
+++ backend/handlers/redis.go
@@ -13,21 +13,34 @@ var Rdb *redis.Client
 var ctx = context.Background()
 
 func InitRedis() {
+	host := os.Getenv("REDIS_HOST")
+	pass := os.Getenv("REDIS_PASSWORD")
+
+	// show in terminal for debugging
+	println("REDIS_HOST =", host)
+	println("REDIS_PASSWORD =", pass)
+
 	Rdb = redis.NewClient(&redis.Options{
-		Addr:     os.Getenv("REDIS_HOST"),
-		Password: os.Getenv("REDIS_PASSWORD"),
+		Addr:     host,
+		Password: pass,
 		DB:       0,
 		TLSConfig: &tls.Config{
 			InsecureSkipVerify: true,
 		},
 	})
+
+	// test connection
+	_, err := Rdb.Ping(ctx).Result()
+	if err != nil {
+		println("PING ERROR =", err.Error())
+	}
 }
 
 func TestRedisHandler(w http.ResponseWriter, r *http.Request) {
-	err := Rdb.Set(ctx, "hello", "world", 0).Err()
+	_, err := Rdb.Ping(ctx).Result()
 	if err != nil {
-		w.Write([]byte("Redis error: " + err.Error()))
+		w.Write([]byte("Ping error: " + err.Error()))
 		return
 	}
-	w.Write([]byte("Redis OK"))
+	w.Write([]byte("Redis connected OK"))
 }
